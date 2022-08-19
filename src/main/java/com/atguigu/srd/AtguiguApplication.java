ab51fb05e595e6e410342c0416eb3ecb0dcc57fa
ipng <572188879@qq.com>
2022-08-20 00:28:30 +0800

    mkdir learngit

---
+++ src/main/java/com/atguigu/srd/AtguiguApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.srd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtguiguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtguiguApplication.class, args);
+	}
+
+}
